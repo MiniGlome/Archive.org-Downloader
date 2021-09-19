8a101d5303f41d96c4300dd76237acd312e6a17d
MiniGlome <54740007+MiniGlome@users.noreply.github.com>
2021-09-19 16:03:49 +0200

    Issue #8 fixed
    Trim the titles to avoid exceeding the 255 chars limit of the files name on macOS

--- downloader.py
+++ downloader.py
@@ -15,6 +15,7 @@ def get_book_infos(session, url):
 	response = session.get(infos_url)
 	data = response.json()['data']
 	title = "".join([c for c in data['brOptions']['bookTitle'] if c in string.ascii_letters+string.digits+" "]).strip()
+	title = title[:250] # Trim the title to avoid exceeding the 255 chars limit of the files name on macOS
 	links = []
 	for item in data['brOptions']['data']:
 		for page in item:
